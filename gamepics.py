694572df7680b6d297d7caea32654d24deab3df9
denmak <denmak@live.ru>
2016-11-27 14:31:09 +0100

    add gamepic with selenium to checkit http://thiagomarzagao.com/2013/11/12/webscraping-with-selenium-part-1/

--- gamepics.py
+++ gamepics.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+# http://thiagomarzagao.com/2013/11/12/webscraping-with-selenium-part-1/  go with this examples 
 driver = webdriver.Chrome("/Users/denmak/Downloads/chromedriver")
 driver.get("http://www.python.org")
 assert "Python" in driver.title
